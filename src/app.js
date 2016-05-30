118845c133eff58f5b7e0e3c7eb7d2fa5b50bf19
Joey Mudd <joseph.mudd@gmail.com>
2016-05-30 14:13:37 -0400

    Join owners table to pets query plus get new aggregate stats

--- src/app.js
+++ src/app.js
@@ -2,9 +2,20 @@
 
 var Sequelize = require('sequelize');
 var nodeDevDbh = new Sequelize('mysql://root:@localhost/nodedev');
-var Pet = nodeDevDbh.define('pet', {}, {tableName: 'pet', freezeTableName: true, createdAt: false, updatedAt: false}); 
-Pet.findAll({attributes: ['id', [Sequelize.fn('group_concat', Sequelize.col('name')),'names']],group: 'ownerId'}).then(function (pets) {
+var Pet = nodeDevDbh.define('pet', {}, {tableName: 'pet', freezeTableName: true, createdAt: false, updatedAt: false});
+var Owner = nodeDevDbh.define('owner', {}, {tableName: 'owner', freezeTableName: true, createdAt: false, updatedAt: false});
+Pet.belongsTo(Owner);
+Owner.hasMany(Pet);
+Pet.findAll({
+include: [{
+  model: Owner,
+  attributes: ["firstName", "lastName"],
+  required: false
+}],
+raw:true,
+attributes: ['id', [Sequelize.literal('SUM(IF(isMale,1,0))'),'numMales'], [Sequelize.literal('SUM(IF(isAlive,1,0))'),'numAlive'],[Sequelize.fn('group_concat', Sequelize.col('name')),'names']],group: 'vetId'
+}).then(function (pets) {
     pets.forEach(function(element,index){
-        console.log(element.get('names'));
+      console.log(element);
     })
 });
