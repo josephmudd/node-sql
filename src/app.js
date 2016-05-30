b792c951616b67cb83d2da5caee39ff2e0ad5011
Joey Mudd <joseph.mudd@gmail.com>
2016-05-30 15:11:12 -0400

    Join third table via second tables FK

--- src/app.js
+++ src/app.js
@@ -4,9 +4,15 @@ var Sequelize = require('sequelize');
 var nodeDevDbh = new Sequelize('mysql://root:@localhost/nodedev');
 var Pet = nodeDevDbh.define('pet', {}, {tableName: 'pet', freezeTableName: true, createdAt: false, updatedAt: false});
 var Owner = nodeDevDbh.define('owner', {}, {tableName: 'owner', freezeTableName: true, createdAt: false, updatedAt: false});
+var Vet = nodeDevDbh.define('vet', {}, {tableName: 'vet', freezeTableName: true, createdAt: false, updatedAt: false});
+
+
 Pet.belongsTo(Owner);
 Owner.hasMany(Pet);
-Pet.findAll({
+Owner.belongsTo(Vet);
+Vet.hasMany(Owner);
+
+/*Pet.findAll({
 include: [{
   model: Owner,
   attributes: ["firstName", "lastName"],
@@ -14,6 +20,25 @@ include: [{
 }],
 raw:true,
 attributes: ['id', [Sequelize.literal('SUM(IF(isMale,1,0))'),'numMales'], [Sequelize.literal('SUM(IF(isAlive,1,0))'),'numAlive'],[Sequelize.fn('group_concat', Sequelize.col('name')),'names']],group: 'vetId'
+}).then(function (pets) {
+    pets.forEach(function(element,index){
+      console.log(element);
+    })
+});
+*/
+
+Vet.findAll({
+include: [{
+  model: Owner,
+  include: [
+    {
+      model: Pet,
+    },
+  ]
+},
+],
+//raw:true,
+attributes: ['vet.*', [Sequelize.literal('SUM(IF(isMale,1,0))'),'numMales'], [Sequelize.literal('SUM(IF(isAlive,1,0))'),'numAlive'],],group: 'vetId'
 }).then(function (pets) {
     pets.forEach(function(element,index){
       console.log(element);
